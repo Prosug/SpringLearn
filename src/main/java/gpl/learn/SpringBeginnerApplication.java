54d1d85aeb4aeb27a3bef38ab2ce283c8404cddf
TopsonOG <gustavopillco01@gmail.com>
2024-09-16 19:17:41 -0400

    my first commint in this repository

---
+++ src/main/java/gpl/learn/SpringBeginnerApplication.java
@@ -0,0 +1,13 @@
+package gpl.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeginnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBeginnerApplication.class, args);
+    }
+
+}
